032c3987724b17310bfee670d5bcdf0d49db80ac
jsdiazs <js.diazs@uniandes.edu.co>
2020-02-05 23:22:57 -0500

    Corrrecciones

--- src/model/logic/Modelo.java
+++ src/model/logic/Modelo.java
@@ -15,95 +15,32 @@ import model.data_structures.IArregloDinamico;
 public class Modelo {
 	
 	
-	private LinkedList<T> linkedList;
+	private LinkedList list;
+	private FCollection g;
 	
 	/**
 	 * Constructor del modelo del mundo con capacidad predefinida
 	 */
 	public Modelo()
 	{
-		linkedList = new LinkedList<T>();
-	}
-	
-	/**
-	 * Constructor del modelo del mundo con capacidad dada
-	 * @param tamano
-	 */
-	public Modelo(int capacidad)
-	{
-		datos = new ArregloDinamico(capacidad);
-	}
-	
-	/**
-	 * Servicio de consulta de numero de elementos presentes en el modelo 
-	 * @return numero de elementos presentes en el modelo
-	 */
-	public int darTamano()
-	{
-		return datos.darTamano();
+		list = new LinkedList();
 	}
 
-	/**
-	 * Requerimiento de agregar dato
-	 * @param dato
-	 */
-	public void agregar(String dato)
-	{	
-		datos.agregar(dato);
-	}
-	
-	/**
-	 * Requerimiento buscar dato
-	 * @param dato Dato a buscar
-	 * @return dato encontrado
-	 */
-	public String buscar(String dato)
-	{
-		return datos.buscar(dato);
-	}
-	
-	/**
-	 * Requerimiento eliminar dato
-	 * @param dato Dato a eliminar
-	 * @return dato eliminado
-	 */
-	public String eliminar(String dato)
-	{
-		return datos.eliminar(dato);
-	}
-	
 
-   
-    	
-    	private FCollection g;
-
-    	private tablaDeHashLinearProbing<String, Feature> TablaHashZn;
-    	/**
-    	 * Constructor del modelo del mundo con capacidad predefinida
-    	 */
-    	public MVCModelo()
-    	{
-    		arbolHora = new ArbolRojoNegro();
-    		arbolSemana = new ArbolRojoNegro();
-    		arbolMes = new ArbolRojoNegro();
-    		//queue = new MaxPQ();
-    		table = new tablaDeHashLinearProbing();
-    	}
-    	
     		//--------------------------------------------------------------------------------------
     		//JSON READER
     	
     	    public  FCollection JSONReader() throws Exception 
     	    {
 
-    	    	FileInputStream inputStream = new FileInputStream("data/bogota_cadastral.json");
+    	    	FileInputStream inputStream = new FileInputStream("data/comparendos_dei_2018_small.geojson");
 
-    	        inputStream  = new FileInputStream("./data/bogota_cadastral.json");
+    	        inputStream  = new FileInputStream("./data/comparendos_dei_2018_small.geojson");
     	        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
     	        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
 
-    	         g = new Gson().fromJson(bufferedReader, FCollection.class);
+    	         g = new Gson.fromJson(bufferedReader, FCollection.class);
     	        
     	        System.out.println("Zonas cargadas por JSON fueron: " +g.features[g.features.length-1].properties.MOVEMENT_ID);
     	        return g;
@@ -142,7 +79,5 @@ public class Modelo {
     	        
     	    }
     	    
-    	   
-
 
 }
